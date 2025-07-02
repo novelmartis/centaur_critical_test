eb51a0ebd69dfd0e10f59e271592a98b6dc1d0fa
Sushrut Thorat <sushrut.thorat94@gmail.com>
2025-07-02 16:25:12 +0200

    update

--- plotting/STM_plot1.py
+++ plotting/STM_plot1.py
@@ -3,8 +3,9 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+condition = 'normal' # normal or superhuman
 
-df = pd.read_csv("STM_trials_80_powers_of_2_perfect_wm.csv")
+df = pd.read_csv("STM_trials_80_powers_of_2_perfect_wm.csv" if condition == 'normal' else "STM_sh_trials_80_powers_of_2_perfect_wm.csv")
 df['n_numbers_in'] = df['n_numbers_in'].astype('category')
 df = df.rename(columns={"n_numbers_in": "Sequence length", "trial": "Trial", "accuracy": "Accuracy"})
 print(df)
@@ -26,5 +27,5 @@ print("means:")
 print(df.groupby('Sequence length')['Accuracy'].mean())
 
 plt.tight_layout()
-plt.savefig("accuracy_by_seq_length_perfect_wm.svg", format='svg')
+plt.savefig("accuracy_by_seq_length_perfect_wm.svg" if condition == 'normal' else "accuracy_by_seq_length_perfect_wm_sh.svg", format='svg', dpi=300)
 # plt.show()
--- plotting/STM_plot2.py
+++ plotting/STM_plot2.py
@@ -3,8 +3,9 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+condition = 'normal' # normal or superhuman
 
-df = pd.read_csv("STM_all_positions_acc_len_64_and_128_perfect_wm.csv")
+df = pd.read_csv("STM_all_positions_acc_len_64_and_128_perfect_wm.csv" if condition == 'normal' else "STM_sh_all_positions_acc_len_64_and_128_perfect_wm.csv")
 df['n_numbers_in'] = df['n_numbers_in'].astype('category')
 df = df.rename(columns={"n_numbers_in": "Sequence length"})
 #df = df[df["Sequence length"] == 64]
@@ -22,5 +23,5 @@ ax = sns.relplot(
 sns.move_legend(ax, "upper right")
 ax.set(xlabel='Digit position', ylabel='Accuracy', ylim=(0.0, 1.0))
 plt.tight_layout()
-plt.savefig("accuracy_by_position_length_all_perfect_wm.svg", format='svg')
+plt.savefig("accuracy_by_position_length_all_perfect_wm.svg" if condition == 'normal' else "accuracy_by_position_length_all_perfect_wm_sh.svg", format='svg', dpi=300)
 # plt.show()
